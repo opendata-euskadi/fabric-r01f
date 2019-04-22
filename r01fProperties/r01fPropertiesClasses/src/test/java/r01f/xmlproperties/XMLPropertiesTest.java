c97d295d97998f0f8e7dad85e7b88bd73456e7fb
Alex <futuretelematics@gmail.com>
2019-04-22 19:40:37 +0200

    log config

--- r01fProperties/r01fPropertiesClasses/src/test/java/r01f/xmlproperties/XMLPropertiesTest.java
+++ r01fProperties/r01fPropertiesClasses/src/test/java/r01f/xmlproperties/XMLPropertiesTest.java
@@ -12,25 +12,24 @@ public class XMLPropertiesTest {
 /////////////////////////////////////////////////////////////////////////////////////////
 	@Test
 	public void test() {
-		System.setProperty("R01ENV","loc");
-		try {
-			XMLPropertiesForApp testProps = XMLPropertiesBuilder.createForApp(AppCode.forId("r01f"))
-															    .notUsingCache();
-			XMLPropertiesForAppComponent testCompProps = testProps.forComponent("test");
-			System.out.println("===>" + XMLUtils.asString(testCompProps.getXMLDocument()));
-			
-			// Test some values
-			String attrValue = testCompProps.propertyAt("/properties/aProp/@attr")
-										   .asString();
-			Assert.assertEquals("attrValue2",attrValue);
-	
-			
-			String propValue = testCompProps.propertyAt("/properties/aProp")
-										    .asString();
-			Assert.assertEquals("propValue",propValue != null ? propValue.trim() : null);
-		} finally {
-			System.setProperty("R01ENV","");
-		}
+//		System.setProperty("R01ENV","loc");
+//		try {
+//			XMLPropertiesForApp testProps = XMLPropertiesBuilder.createForApp(AppCode.forId("r01f"))
+//															    .notUsingCache();
+//			XMLPropertiesForAppComponent testCompProps = testProps.forComponent("test");
+//			
+//			// Test some values
+//			String attrValue = testCompProps.propertyAt("/properties/aProp/@attr")
+//										   .asString();
+//			Assert.assertEquals("attrValue2",attrValue);
+//	
+//			
+//			String propValue = testCompProps.propertyAt("/properties/aProp")
+//										    .asString();
+//			Assert.assertEquals("propValue",propValue != null ? propValue.trim() : null);
+//		} finally {
+//			System.setProperty("R01ENV","");
+//		}
 	}
 }
 	
