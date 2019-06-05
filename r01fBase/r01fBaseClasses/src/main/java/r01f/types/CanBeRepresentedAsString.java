4bc71e288946d7681ce69804b3179276d5583b4a
Israel Garcia <i-garciaalesanco@ejie.eus>
2019-06-05 11:09:21 +0200

    Serializable

--- r01fBase/r01fBaseClasses/src/main/java/r01f/types/CanBeRepresentedAsString.java
+++ r01fBase/r01fBaseClasses/src/main/java/r01f/types/CanBeRepresentedAsString.java
@@ -1,8 +1,11 @@
 package r01f.types;
 
+import java.io.Serializable;
+
 /**
  * Interface for types that can be represented as a {@link String}
  */
-public interface CanBeRepresentedAsString {
+public interface CanBeRepresentedAsString
+		 extends Serializable {
 	public String asString();
 }
