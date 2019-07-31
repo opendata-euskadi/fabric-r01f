60a94691c0f7d4b6715270eb9d94fa6c3ed66491
PCI <pci@ejie.eus>
2019-07-31 02:57:17 +0200

    Refactorings

--- r01fBase/r01fBaseClasses/src/main/java/r01f/patterns/FactoryFrom.java
+++ r01fBase/r01fBaseClasses/src/main/java/r01f/patterns/FactoryFrom.java
@@ -2,11 +2,14 @@ package r01f.patterns;
 
 /**
  * Interface for factory types
+ * @param <S> the source type
  * @param <T> the type to create
  */
-public interface FactoryFrom<O,T> {
+public interface FactoryFrom<S,T> {
 	/**
+	 * Creates an object instance using the given object
+	 * @param source
 	 * @return an instance of type T built from other
 	 */
-	public T from(final O other);
+	public T from(final S source);
 }
