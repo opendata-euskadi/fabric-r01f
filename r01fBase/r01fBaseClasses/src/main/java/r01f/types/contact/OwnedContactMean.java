a96b9f0fd5fe8285c63127c302678831c63612a6
PCI <pci@ejie.eus>
2019-08-22 13:22:17 +0200

    Added CanBeConvertedAsString facet to OwnedContactMean

--- r01fBase/r01fBaseClasses/src/main/java/r01f/types/contact/OwnedContactMean.java
+++ r01fBase/r01fBaseClasses/src/main/java/r01f/types/contact/OwnedContactMean.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
+import r01f.types.CanBeRepresentedAsString;
 
 /**
  * Represents a {@link ContactMean} like a {@link Phone} or an {@link EMail}
@@ -14,7 +15,8 @@ import lombok.experimental.Accessors;
 @Accessors(prefix="_")
 @RequiredArgsConstructor
 public class OwnedContactMean<M extends ContactMean>
-  implements Serializable {
+  implements CanBeRepresentedAsString,
+  			 Serializable {
 
 	private static final long serialVersionUID = -1135076268050723452L;
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -22,4 +24,18 @@ public class OwnedContactMean<M extends ContactMean>
 /////////////////////////////////////////////////////////////////////////////////////////
 	@Getter private final M _id;
 	@Getter private final String _owner;
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+	@Override
+	public String asString() {
+        // rfc822 format
+        return String.format("\"%s\" <%s>",
+        					 _owner != null ? _owner : _id.asString(),_id.asString());
+	}
+	@Override
+	public String toString() {
+		return this.asString();
+	}
 }
