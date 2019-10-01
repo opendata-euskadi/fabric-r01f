0ca78b46bfeac1ec01a912032a8e6cb29da65136
PCI <pci@ejie.eus>
2019-10-01 15:48:04 +0200

    Aspects project moved to it's own repo

--- r01fAspect/r01fLazyLoadAspect/r01fLazyLoadInterfaceClasses/src/main/java/r01f/aspects/interfaces/lazyload/LazyLoadedTypeSupplier.java
+++
@@ -1,9 +0,0 @@
-package r01f.aspects.interfaces.lazyload;
-
-/**
- * Marker interface for Lazily-Loaded objects
- * @param <T> the supplied type
- */
-public interface LazyLoadedTypeSupplier<T> {
-	/* a marker interface */
-}
