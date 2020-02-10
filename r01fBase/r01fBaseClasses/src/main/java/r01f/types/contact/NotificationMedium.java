7921d9553ebd1455434d3b1ba08d6b20f9bf8545
iolabaro <iolabaro@B6001241.elkarlan.euskadi.eus>
2020-02-10 09:27:53 +0100

    minor changes

--- r01fBase/r01fBaseClasses/src/main/java/r01f/types/contact/NotificationMedium.java
+++ r01fBase/r01fBaseClasses/src/main/java/r01f/types/contact/NotificationMedium.java
@@ -3,21 +3,22 @@ package r01f.types.contact;
 import r01f.enums.EnumExtended;
 import r01f.enums.EnumExtendedWrapper;
 
-public enum NotificationMedium 
+public enum NotificationMedium
  implements EnumExtended<NotificationMedium> {
 	EMAIL,
 	SMS,
 	VOICE,
-	LOG;
+	LOG,
+	PUSH_MESSAGE;
 
 	public String asStringLowerCase() {
 		return this.name().toLowerCase();
 	}
 /////////////////////////////////////////////////////////////////////////////////////////
-//	                                                                          
-/////////////////////////////////////////////////////////////////////////////////////////		
+//
+/////////////////////////////////////////////////////////////////////////////////////////
 	private static final transient EnumExtendedWrapper<NotificationMedium> DELEGATE = EnumExtendedWrapper.wrapEnumExtended(NotificationMedium.class);
-	
+
 	@Override
 	public boolean isIn(final NotificationMedium... els) {
 		return DELEGATE.isIn(this,els);
