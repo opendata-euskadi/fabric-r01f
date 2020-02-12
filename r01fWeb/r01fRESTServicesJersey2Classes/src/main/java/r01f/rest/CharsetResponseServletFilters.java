2f0dd3bb586cd854c2023e9fe63633f826f91380
iolabaro <iolabaro@B6001241.elkarlan.euskadi.eus>
2020-02-12 11:09:14 +0100

    Charset for rest

---
+++ r01fWeb/r01fRESTServicesJersey2Classes/src/main/java/r01f/rest/CharsetResponseServletFilters.java
@@ -0,0 +1,65 @@
+package r01f.rest;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+import lombok.experimental.Accessors;
+import r01f.util.types.Strings;
+
+/**
+ * To use  CharsetResponseServletFilters:
+ * 	 1. Anotate  at REST Resource: @CharsetResponseFilter
+ *   2. Register at REST Bootstraping :
+ */
+public abstract  class CharsetResponseServletFilters {
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CharsetResponseServletFilterBase
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+	@Accessors(prefix="_")
+	public static abstract class CharsetResponseServletFilterBase
+				implements ContainerResponseFilter,
+						   RESTFilter {
+		private final String _charset;
+
+		public CharsetResponseServletFilterBase(final String charset) {
+			_charset = charset;
+		}
+	    @Override
+	    public void filter(ContainerRequestContext request, ContainerResponseContext response) {
+	        MediaType type = response.getMediaType();
+	        if (type != null) {
+	            String contentType = type.toString();
+	            if (!contentType.contains("charset")) {
+	                String contentTypeAsString = Strings.customized("{};charset={}", contentType,_charset);
+	                response.getHeaders().putSingle("Content-Type", contentTypeAsString);
+	            }
+	        }
+	    }
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//ISOCharsetResponseServletFilter
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+	  @Provider
+	  @CharsetResponseFilter
+	  public static  class ISOCharsetResponseServletFilter
+	  			extends CharsetResponseServletFilterBase {
+		  public ISOCharsetResponseServletFilter() {
+			  super("iso-8859-1");
+		  }
+	  }
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//UTF8CharsetResponseServletFilter
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+	  @Provider
+	  @CharsetResponseFilter
+	  public static  class UTF8CharsetResponseServletFilter
+	  			extends CharsetResponseServletFilterBase {
+		  public UTF8CharsetResponseServletFilter() {
+			  super("utf8");
+		  }
+	  }
+
+}
