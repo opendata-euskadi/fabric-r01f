94a72fe0f531f8d4186abd4a4b519ebff265c694
pci <pci@ejie.eus>
2020-12-16 18:27:59 +0100

    SecurityContext for ApiKey

--- r01fBase/r01fBaseClasses/src/main/java/r01f/securitycontext/SecurityContextForApiKey.java
+++ r01fBase/r01fBaseClasses/src/main/java/r01f/securitycontext/SecurityContextForApiKey.java
@@ -1,7 +1,9 @@
 package r01f.securitycontext;
 
+import r01f.securitycontext.SecurityOIDs.LoginOID;
 import r01f.types.AppVersion;
 
 public interface SecurityContextForApiKey {
+	public LoginOID getApiKeyOid();
 	public AppVersion getApiKeyVersion();
 }
