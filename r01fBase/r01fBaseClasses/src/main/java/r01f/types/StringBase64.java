2585e5a937f3e97658282474365cba3e3416e806
pci <pci@ejie.eus>
2020-12-23 21:47:34 +0100

    StringBase64 > add encode / decode functionality

--- r01fBase/r01fBaseClasses/src/main/java/r01f/types/StringBase64.java
+++ r01fBase/r01fBaseClasses/src/main/java/r01f/types/StringBase64.java
@@ -1,26 +1,97 @@
 package r01f.types;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterOutputStream;
 
 import com.google.common.base.Function;
 
-import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import r01f.objectstreamer.annotations.MarshallType;
 import r01f.util.types.StringEncodeUtils;
 
 @MarshallType(as="stringBase64")
-@RequiredArgsConstructor
-public class StringBase64 
+public class StringBase64
   implements Serializable,
   			 CanBeRepresentedAsString {
 
 	private static final long serialVersionUID = -4115901126395440560L;
 /////////////////////////////////////////////////////////////////////////////////////////
-//	
+//	CONSTANTS
+/////////////////////////////////////////////////////////////////////////////////////////
+	public static final Function<String,String> DEFAULT_ENCODER_FUNCION = new Function<String,String>() {
+																					@Override
+																					public String apply(final String input) {
+																						return StringEncodeUtils.encodeBase64AsString(input.getBytes());
+																					}
+																		  };
+/////////////////////////////////////////////////////////////////////////////////////////
+//	FIELDS
 /////////////////////////////////////////////////////////////////////////////////////////
 	private final String _stringBase64;
+
 /////////////////////////////////////////////////////////////////////////////////////////
-//	
+//	CONSTRUCTOR & BUILDER
+/////////////////////////////////////////////////////////////////////////////////////////
+	public StringBase64(final String stringBase64) {
+		_stringBase64 = stringBase64;
+	}
+	public static StringBase64 fromBase64EncodedString(final String stringBase64) {
+		return new StringBase64(stringBase64);
+	}
+	public static StringBase64 encode(final String rawString) {
+		return StringBase64.encode(rawString,
+								   DEFAULT_ENCODER_FUNCION);
+	}
+	public static StringBase64 encode(final String rawString,
+							  		  final Function<String,String> encoder) {
+		if (rawString == null || encoder == null) throw new IllegalArgumentException();
+		return new StringBase64(encoder.apply(rawString));
+	}
+	public static StringBase64 encode(final byte[] rawBytes) {
+		if (rawBytes == null || rawBytes.length == 0) throw new IllegalArgumentException();
+ 		return new StringBase64(StringEncodeUtils.encodeBase64AsString(rawBytes));
+	}
+	@SneakyThrows
+	public static StringBase64 deflateAndEncode(final String rawString) {
+		return StringBase64.deflateAndEncode(rawString,
+								   			 DEFAULT_ENCODER_FUNCION);
+	}
+	@SneakyThrows
+	public static StringBase64 deflateAndEncode(final String rawString,
+												final Function<String,String> encoder) {
+		if (rawString == null || encoder == null) throw new IllegalArgumentException();
+
+		// [1] - Deflate
+		byte[] deflatedRawBytes = _deflate(rawString.getBytes());
+
+		// [2] - Encode base 64
+		return StringBase64.encode(encoder.apply(new String(deflatedRawBytes)));
+	}
+	@SneakyThrows
+	public static StringBase64 deflateAndEncode(final byte[] rawBytes) {
+		// [1] - Deflate
+		byte[] deflatedRawBytes = _deflate(rawBytes);
+
+		// [2] - Encode base 64
+		return StringBase64.encode(deflatedRawBytes);
+	}
+/////////////////////////////////////////////////////////////////////////////////////////
+//	AS STRING
+/////////////////////////////////////////////////////////////////////////////////////////
+	@Override
+	public String asString() {
+		return _stringBase64;
+	}
+	public byte[] getBytes() {
+		return _stringBase64.getBytes();
+	}
+/////////////////////////////////////////////////////////////////////////////////////////
+//	DECODE
 /////////////////////////////////////////////////////////////////////////////////////////
 	public String decode() {
 		return StringEncodeUtils.decodeBase64(_stringBase64)
@@ -28,37 +99,53 @@ public class StringBase64
 	}
 	public byte[] decodeAsBytes() {
 		return StringEncodeUtils.decodeBase64(_stringBase64.getBytes());
-								
 	}
-	
-	public static StringBase64 encode(final String rawString) {
-		if (rawString == null) throw new IllegalArgumentException();
-		return StringBase64.encode(rawString,
-								   new Function<String,String>() {
-											@Override
-											public String apply(final String input) {
-												return StringEncodeUtils.encodeBase64String(input.getBytes());
-											}
-								   });
+	@SneakyThrows
+	public String decodeAndInflate() {
+		// [1] - Decode
+		String deflatedString = this.decode();
+
+		// [2] - Inflate
+		byte[] inflatedBytes = _inflate(deflatedString.getBytes());
+		return new String(inflatedBytes);
 	}
-	public static StringBase64 encode(final String rawString,
-							  		  final Function<String,String> encoder) {
-		if (rawString == null || encoder == null) throw new IllegalArgumentException();
-		return new StringBase64(encoder.apply(rawString));
-	}
-	public static StringBase64 encode(final byte[] bytes) {
-		if (bytes.length == 0) throw new IllegalArgumentException();
-		return new StringBase64(StringEncodeUtils.encodeBase64String(bytes));
+	@SneakyThrows
+	public byte[] decodeAsBytesAndInflate() {
+		// [1] - Decode
+		byte[] deflatedBytes = this.decodeAsBytes();
+
+		// [2] - Inflate
+		byte[] inflatedBytes = _inflate(deflatedBytes);
+		return inflatedBytes;
 	}
 /////////////////////////////////////////////////////////////////////////////////////////
-//	
+//	INFLATE / DEFLATE
 /////////////////////////////////////////////////////////////////////////////////////////
-	@Override
-	public String asString() {
-		return _stringBase64;
+	private static byte[] _deflate(final byte[] inflatedBytes) throws IOException{
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		Deflater deflater = new Deflater(Deflater.BEST_COMPRESSION,
+										 true);	// use a GZIP compatible version
+		DeflaterOutputStream deflaterOutputStream = new DeflaterOutputStream(outputStream,
+																			 deflater);
+		deflaterOutputStream.write(inflatedBytes);
+		deflaterOutputStream.close();
+		byte[] outDeflatedStringBytes = outputStream.toByteArray();
+
+		return outDeflatedStringBytes;
+	}
+	private static byte[] _inflate(final byte[] deflatedBytes) throws IOException {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		Inflater inflater = new Inflater(true);
+		InflaterOutputStream inflaterOutputStream = new InflaterOutputStream(outputStream,
+																			 inflater);
+		inflaterOutputStream.write(deflatedBytes);
+		inflaterOutputStream.close();
+		byte[] outInflatedStringBytes = outputStream.toByteArray();
+
+		return outInflatedStringBytes;
 	}
 /////////////////////////////////////////////////////////////////////////////////////////
-//	
+//	EQUALS
 /////////////////////////////////////////////////////////////////////////////////////////
 	@Override
 	public boolean equals(final Object obj) {
