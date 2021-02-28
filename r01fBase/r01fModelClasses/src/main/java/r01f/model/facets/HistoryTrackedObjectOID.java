d12a2f4a80ca3fa84a27fff7f01378dd56f2499b
pci <pci@ejie.eus>
2021-02-28 23:02:17 +0100

    History > new historyTrackedObjectOID facet

---
+++ r01fBase/r01fModelClasses/src/main/java/r01f/model/facets/HistoryTrackedObjectOID.java
@@ -0,0 +1,10 @@
+package r01f.model.facets;
+
+import r01f.guids.OIDTyped;
+import r01f.guids.PersistableObjectOID;
+
+public interface HistoryTrackedObjectOID 
+		 extends OIDTyped<String>,
+		 		 PersistableObjectOID {
+	// a marker interface for history tracked objects
+}
